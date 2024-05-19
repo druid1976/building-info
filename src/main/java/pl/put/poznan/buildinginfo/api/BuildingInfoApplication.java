2538406218f09f2171c7d33e5425d5b7cb6ff012
druid1976 <erenkaanoral@hotmail.com>
2024-05-19 23:46:39 +0200

    jesus take the wheel x3

--- src/main/java/pl/put/poznan/buildinginfo/api/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildinginfo/api/BuildingInfoApplication.java
@@ -2,7 +2,10 @@ package pl.put.poznan.buildinginfo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
+import java.io.IOException;
 
 @SpringBootApplication
 public class BuildingInfoApplication {
@@ -11,5 +14,35 @@ public class BuildingInfoApplication {
         SpringApplication.run(BuildingInfoApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void openBrowser() {
+        String url = "http://localhost:8080/buildings";
+        try {
+            if (isWindows()) {
+                Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start " + url});
+            } else if (isMac()) {
+                Runtime.getRuntime().exec(new String[]{"open", url});
+            } else if (isUnix()) {
+                Runtime.getRuntime().exec(new String[]{"xdg-open", url});
+            } else {
+                System.err.println("Unsupported OS");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+    private static boolean isMac() {
+        return System.getProperty("os.name").toLowerCase().contains("mac");
+    }
+
+    private static boolean isUnix() {
+        return System.getProperty("os.name").toLowerCase().contains("nix") ||
+                System.getProperty("os.name").toLowerCase().contains("nux") ||
+                System.getProperty("os.name").toLowerCase().contains("aix");
+    }
 }
