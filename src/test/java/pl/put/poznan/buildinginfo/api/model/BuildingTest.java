883096c61730935a9db8d8cce0c87f7ab5feb343
Alp Çağan <Alp@Ilhan-MacBook-Air.local>
2024-05-29 00:23:57 +0200

    added 2 test files for room and building class

---
+++ src/test/java/pl/put/poznan/buildinginfo/api/model/BuildingTest.java
@@ -0,0 +1,48 @@
+package pl.put.poznan.buildinginfo.api.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildingTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getLevels() {
+    }
+
+    @Test
+    void setLevels() {
+    }
+
+    @Test
+    void calculateArea() {
+    }
+
+    @Test
+    void calculateVolume() {
+    }
+
+    @Test
+    void calculateLightingPower() {
+    }
+
+    @Test
+    void calculateHeatingEnergy() {
+    }
+}
---
+++ src/test/java/pl/put/poznan/buildinginfo/api/model/RoomTest.java
@@ -0,0 +1,48 @@
+package pl.put.poznan.buildinginfo.api.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void setArea() {
+    }
+
+    @Test
+    void getCube() {
+    }
+
+    @Test
+    void setCube() {
+    }
+
+    @Test
+    void getLight() {
+    }
+
+    @Test
+    void setLight() {
+    }
+}
