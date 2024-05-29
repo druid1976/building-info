67f7b6403c3d9c38174affc8d63533971bc32def
Alp Çağan <alpcaganilhan@hotmail.com>
2024-05-29 02:21:12 +0200

    room test initiaded

--- src/test/java/pl/put/poznan/buildinginfo/api/model/RoomTest.java
+++ src/test/java/pl/put/poznan/buildinginfo/api/model/RoomTest.java
@@ -8,41 +8,71 @@ class RoomTest {
 
     @Test
     void getId() {
+        Room room = new Room();
+        room.setId("101");
+        assertEquals("101", room.getId());
     }
 
     @Test
     void setId() {
+        Room room = new Room();
+        room.setId("101");
+        assertEquals("101", room.getId());
     }
 
     @Test
     void getName() {
+        Room room = new Room();
+        room.setName("Room 101");
+        assertEquals("Room 101", room.getName());
     }
 
     @Test
     void setName() {
+        Room room = new Room();
+        room.setName("Room 101");
+        assertEquals("Room 101", room.getName());
     }
 
     @Test
     void getArea() {
+        Room room = new Room();
+        room.setArea(50.0);
+        assertEquals(50.0, room.getArea());
     }
 
     @Test
     void setArea() {
+        Room room = new Room();
+        room.setArea(50.0);
+        assertEquals(50.0, room.getArea());
     }
 
     @Test
     void getCube() {
+        Room room = new Room();
+        room.setCube(100.0);
+        assertEquals(100.0, room.getCube());
     }
 
     @Test
     void setCube() {
+        Room room = new Room();
+        room.setCube(100.0);
+        assertEquals(100.0, room.getCube());
     }
 
     @Test
     void getLight() {
+        Room room = new Room();
+        room.setLight(500.0);
+        assertEquals(500.0, room.getLight());
     }
 
     @Test
     void setLight() {
+        Room room = new Room();
+        room.setLight(500.0);
+        assertEquals(500.0, room.getLight());
     }
 }
